eb1d828f538c9a4c404c43703f050f240b65f094
Seyrit <seyrit16@mail.ru>
2024-07-01 10:32:41 +0300

    creat

---
+++ src/main/java/com/ocode/cbrf/CbrfApplication.java
@@ -0,0 +1,13 @@
+package com.ocode.cbrf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbrfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbrfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ocode/cbrf/CbrfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocode.cbrf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbrfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
