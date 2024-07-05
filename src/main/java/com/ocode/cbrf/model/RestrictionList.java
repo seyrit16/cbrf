04fd0d376006d555befd4a6b7ea9ff014ea29e04
Seyrit <seyrit16@mail.ru>
2024-07-05 14:03:37 +0300

    Create RestrictionList.java

---
+++ src/main/java/com/ocode/cbrf/model/RestrictionList.java
@@ -0,0 +1,34 @@
+package com.ocode.cbrf.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.lang.NonNull;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "restriction_list")
+public class RestrictionList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @Column(name = "restriction")
+    private String restriction;
+
+    @NonNull
+    @Column(name = "date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    @ManyToMany(mappedBy = "restrictionList")
+    private List<ParticipantInfo> participantInfos;
+}
