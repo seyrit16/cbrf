37be79067f366053fd9065a5ede7bec5fdfd1339
Seyrit <seyrit16@mail.ru>
2024-07-06 10:53:10 +0300

    Update SWBICS.java

--- src/main/java/com/ocode/cbrf/model/SWBICS.java
+++ src/main/java/com/ocode/cbrf/model/SWBICS.java
@@ -1,15 +1,29 @@
 package com.ocode.cbrf.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.lang.NonNull;
 
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "swbics")
 public class SWBICS {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NonNull
+    @Column(name = "swbic")
+    private String swbic;
+
+    @Column(name = "default_swbic")
+    private boolean defaultSWBIC;
 
     @ManyToMany(mappedBy = "swbics")
     private List<BICDirectoryEntry> bicDirectoryEntries;
