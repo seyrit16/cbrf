2f552ef986d6820eb05222040ffa3fdc7100456a
Seyrit <seyrit16@mail.ru>
2024-07-09 13:21:05 +0300

    Update BICDirectoryEntryRepository.java

--- src/main/java/com/ocode/cbrf/repository/BICDirectoryEntryRepository.java
+++ src/main/java/com/ocode/cbrf/repository/BICDirectoryEntryRepository.java
@@ -7,12 +7,13 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BICDirectoryEntryRepository extends JpaRepository<BICDirectoryEntry, Long> {
     @Query(value = "select * from bic_directory_entry", nativeQuery = true)
     List<BICDirectoryEntry> findAll();
 
-    @Query(value = "select * from bic_directory_entry as bde where bde.bic = :bic",nativeQuery = true)
-    BICDirectoryEntry findByBic(@Param("bic") Integer bic);
+    @Query(value = "select * from bic_directory_entry as bde where bde.bic = :bic limit 1",nativeQuery = true)
+    Optional<BICDirectoryEntry> findByBic(@Param("bic") Integer bic);
 }
