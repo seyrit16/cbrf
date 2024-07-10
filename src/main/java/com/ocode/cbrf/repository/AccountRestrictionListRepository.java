c70531f3a5c541afd8d489739f9f1dd5afabf3ec
Seyrit <seyrit16@mail.ru>
2024-07-10 08:46:40 +0300

    Create AccountRestrictionListRepository.java

---
+++ src/main/java/com/ocode/cbrf/repository/AccountRestrictionListRepository.java
@@ -0,0 +1,25 @@
+package com.ocode.cbrf.repository;
+
+import com.ocode.cbrf.model.AccountRestrictionList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AccountRestrictionListRepository extends JpaRepository<AccountRestrictionList, Long> {
+    @Query(value = "select * from account_restriction_list", nativeQuery = true)
+    List<AccountRestrictionList> findAll();
+
+    @Query(value = "select * from account_restriction_list as arl where arl.id = :id limit 1", nativeQuery = true)
+    Optional<AccountRestrictionList> findById(@Param("id") Long id);
+
+    @Query(value = "select *" +
+            " from account_restriction_list as arl" +
+            " where arl.account_restriction = :accountRestriction" +
+            " limit 1", nativeQuery = true)
+    Optional<AccountRestrictionList> findByAccountRestriction(@Param("accountRestriction") String accountRestriction);
+}
