b8cfe379500543d1a0ebc86a3317a970d20ee4b8
Matthew <privalka2003@mail.ru>
2024-07-19 18:55:41 +0400

    create repositories for user and role

---
+++ src/main/java/com/ocode/cbrf/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.ocode.cbrf.repository;
+
+import com.ocode.cbrf.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(value = "select * from cbrf_user as u where u.login = :login", nativeQuery = true)
+    Optional<User> findUserByLogin(@Param("login") String login);
+}
