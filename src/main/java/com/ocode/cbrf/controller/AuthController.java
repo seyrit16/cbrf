ddeebd603865b56988fc67a55998499e24926a85
Matthew <privalka2003@mail.ru>
2024-08-07 01:52:40 +0400

    add authentication controller

---
+++ src/main/java/com/ocode/cbrf/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.ocode.cbrf.controller;
+
+import com.ocode.cbrf.dto.impl.user.http.JwtAuthenticationResponse;
+import com.ocode.cbrf.dto.impl.user.http.SignInRequest;
+import com.ocode.cbrf.dto.impl.user.http.SignUpRequest;
+import com.ocode.cbrf.service.web.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthenticationService authenticationService;
+
+    @Autowired
+    public AuthController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/sign-up")
+    public JwtAuthenticationResponse signUp(@RequestBody SignUpRequest request) {
+        return authenticationService.signUp(request);
+    }
+
+    @PostMapping("/sign-in")
+    public JwtAuthenticationResponse signIn(@RequestBody SignInRequest request) {
+        return authenticationService.signIn(request);
+    }
+}
