404756b93593eb8d9917ebbbd0a0caeec30ab1c6
Seyrit <seyrit16@mail.ru>
2024-08-21 00:49:52 +0300

    Create ConflictDataException.java

---
+++ src/main/java/com/ocode/cbrf/exception/ConflictDataException.java
@@ -0,0 +1,7 @@
+package com.ocode.cbrf.exception;
+
+public class ConflictDataException extends RuntimeException{
+    public ConflictDataException(String message){
+        super(message);
+    }
+}
