f1424b1f230f85d8b271a65f0d073883fa5ca46c
Seyrit <seyrit16@mail.ru>
2024-08-21 16:11:37 +0300

    remove dtoservice

--- src/main/java/com/ocode/cbrf/service/DtoService.java
+++
@@ -1,8 +0,0 @@
-package com.ocode.cbrf.service;
-
-import com.ocode.cbrf.dto.impl.ED807Dto;
-import com.ocode.cbrf.model.ED807;
-
-public interface DtoService {
-    ED807 toEntities(ED807Dto ed807Dto);
-}
